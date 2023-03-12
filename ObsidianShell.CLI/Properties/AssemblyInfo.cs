e9805682abf55980baf920b8a0993e99e7b84206
Chaoses-Ib <Chaos-es@outlook.com>
2023-03-12 20:14:48 +0800

    Add icon to GUI and bump version

--- ObsidianShell.CLI/Properties/AssemblyInfo.cs
+++ ObsidianShell.CLI/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ObsidianShell.CLI")]
-[assembly: AssemblyDescription("A command line interface program for opening .md files in Obsidian")]
+[assembly: AssemblyDescription("A command line interface program for opening Markdown files in Obsidian")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("https://github.com/Chaoses-Ib/ObsidianShell")]
 [assembly: AssemblyProduct("ObsidianShell")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.1.0")]
-[assembly: AssemblyFileVersion("0.3.1.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
--- docs/README.md
+++ docs/README.md
@@ -1,8 +1,9 @@
 ## Release
 1. Bump version
-   - `CLI/Properties/AssemblyInfo.cs`
-   - `ContextMenu/Properties/AssemblyInfo.cs`
+   - `ObsidianShell.CLI/Properties/AssemblyInfo.cs`
+   - `ObsidianShell.ContextMenu/Properties/AssemblyInfo.cs`
+   - `ObsidianShell.GUI/Properties/AssemblyInfo.cs`
 2. Build the solution
-3. Copy files from `CLI\bin\Release` and `ContextMenu\bin\Release` to `Installer\bin` (without PDB and XML files)
+3. Copy files from `bin\Release` to `Installer\bin` (without PDB and XML files)
 4. Build the installer by Advanced Installer
 5. Pack files in `Installer\bin` as the protable version (in a directory named `ObsidianShell`)
